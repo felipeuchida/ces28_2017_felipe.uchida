1e4515001a88cfa084d4c3914ebf8c9347262fc5
felipeuchida <felipeuchida1@gmail.com>
2017-09-19 08:36:00 -0300

    Lab 02 - Tarefa 06 à 09
    Já levando em conta alguns casos especiais

--- labs/Lab02/Lab2_TDD_Pair_Programming/Test/CalculadoraStringTest.java
+++ labs/Lab02/Lab2_TDD_Pair_Programming/Test/CalculadoraStringTest.java
@@ -5,7 +5,6 @@ import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
 public class CalculadoraStringTest {
-/*
 	@Test
 	// Teste para a tarefa 1
 	public void WhenEmptyThenReturnsZero() {
@@ -33,7 +32,7 @@ public class CalculadoraStringTest {
 		assertEquals(CalculadoraString.add(" ,,   ,,,,,,,  "),0);
 		assertEquals(CalculadoraString.add(" ,,   ,,,,,5,,  "),5);
 	}
-
+	
 	@Test
 	// Teste para a tarefa 3
 	public void WhenUseNewLineThenReturnsSum() {
@@ -41,7 +40,7 @@ public class CalculadoraStringTest {
 		assertEquals(CalculadoraString.add("1,\n"),1);
 		assertEquals(CalculadoraString.add(" ,,  \n\n 22\n\n\n,,,,,5,, \n "),27);
 	}
-
+	
 	@Test
 	// Teste para a tarefa 4
 	public void WhenUseDifferentDelimiterThenReturnsSum() {
@@ -50,7 +49,7 @@ public class CalculadoraStringTest {
 		assertEquals(CalculadoraString.add("//[;]\n1;2"),3);
 		assertEquals(CalculadoraString.add("//[;]\n1;2,3\n4"),10);
 	}
-	*/
+	
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 	
@@ -68,5 +67,108 @@ public class CalculadoraStringTest {
 		expectedException.expect(IllegalArgumentException.class);
 		expectedException.expectMessage("negativos proibidos [-1445 -3166 -3]");
 		CalculadoraString.add("-00001445 448,,,,, -003166 555555 -03");
+	}
+
+	@Test
+	// Teste para a tarefa 6
+	public void WhenNumberIsBiggerThen1000ThenIgnoreIt() {
+		assertEquals(CalculadoraString.add("//[;]\n1;2 1001 4"),7);
+		assertEquals(CalculadoraString.add("//[;]\n1;2, 10000 3\n4"),10);
+		assertEquals(CalculadoraString.add(" ,1001,, 99999,, "),0);
+	}
+	
+	@Test
+	// Teste para a tarefa 7
+	public void WhenUseDifferentDelimiterWithAnyLengthThenReturnsSum() {
+		assertEquals(CalculadoraString.add("//[d+]\n"),0);
+		assertEquals(CalculadoraString.add("//[***]\n1***2***3"),6);
+		assertEquals(CalculadoraString.add("//[***]\n1***2,3\n4"),10);
+	}
+	
+	@Test
+	// Teste para a tarefa 8
+	public void WhenUseDifferentDelimitersThenReturnsSum() {
+		assertEquals(CalculadoraString.add("//[*][%]\n1*2%3"),6);
+		assertEquals(CalculadoraString.add("//[*][%]\n1*2%3\n4,5"),15);
+		assertEquals(CalculadoraString.add("//[*][%][a][b]\n1*2%3\n4,5aaa10bbb90"),115);
+	}
+	
+	@Test
+	// Teste para a tarefa 9
+	public void WhenUseDifferentDelimitersWithAnyLengthThenReturnsSum() {
+		assertEquals(CalculadoraString.add("//[***][%]\n1***2%3"),6);
+		assertEquals(CalculadoraString.add("//[*asdf][%asdf]\n1*asdf2%asdf3\n4,5"),15);
+		assertEquals(CalculadoraString.add("//[*][%][aa][bb]\n1*2%3\n4,5aaaa10bbbb90"),115);
+	}
+	
+	@Test
+	// Teste para o FAQ e Casos Especiais 1
+	// Se a entrada começar com "//[***]\n", então desconsidero * e ** como delimitadores ou *, ** e *** serão delimitadores?
+	public void WhenUseDifferentDelimitersWithAnyLengthThenSubstringsOfThisDelimitersArentDelimiter() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Entrada inválida e não-tratável");
+		CalculadoraString.add("//[***]\n1*2**3");
+	}
+	
+	@Test
+	// Teste para o FAQ e Casos Especiais 2
+	// Os colchetes, '[' , ']'  podem ser delimitadores?
+	public void WhenUseOpenerBracketAsDelimiterThenException() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Entrada inválida e não-tratável");
+		CalculadoraString.add("//[[]\n");
+	}
+	
+	@Test
+	// Teste para o FAQ e Casos Especiais 2
+	public void WhenUseClosenerBracketAsDelimiterThenException() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Entrada inválida e não-tratável");
+		CalculadoraString.add("//[]]\n");
+	}
+	
+	@Test
+	// Teste para o FAQ e Casos Especiais 3
+	// Caracteres que são digitos, e.g. ‘2’'  podem ser delimitadores?
+	public void WhenUseNumberAsDelimiterThenException() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Entrada inválida e não-tratável");
+		CalculadoraString.add("//[3]\n");
+	}
+	
+	@Test
+	// Teste para o FAQ e Casos Especiais 4
+	// Tab eh diferente de espaço em branco?
+	public void WhenDontDefineTabAsDelimiterThenExceptionIfUsed() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Entrada inválida e não-tratável");
+		CalculadoraString.add("//[*]****\t");
+	}
+	
+	@Test
+	// Teste para o FAQ e Casos Especiais 5
+	/* "//[-]\n -1--2- -3- \n-4-,-5" devo retornar apenas uma excecao ou retorno uma excecao com a mensagem 
+ 	"negativos proibidos [-2 -3 -4 -5]" ou seria "negativos proibidos [-2]" ou seria "negativos proibidos [-2 -3]"?*/
+	public void WhenDefineMinusAsDelimiterThenMinusIsLikeSpace() {
+		assertEquals(CalculadoraString.add("//[-]\n -1--2- -3- \n-4-,-5"),CalculadoraString.add("1,2,3,4,5"));
+		assertEquals(CalculadoraString.add("//[-]\n -1--2- -3- \n-4-,-5"),15);
+	}
+	
+	//@Test
+	// Teste para o FAQ e Casos Especiais 6
+	/*" -0 -1001 -2 -2" retorno uma excecao com a mensagem "negativos proibidos [-2]" ou "negativos proibidos [ -0 -2 -1001]" 
+	 * ou "negativos proibidos [-2 -1001]" ou "negativos proibidos [-1001 -2]" ou "negativos proibidos [-0 -1001 -2 -2]" ou uma outra forma?*/
+	/*public void WhenNumberIsSmallerThenMinus1000ThenAlsoHasPartOnTheException() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("negativos proibidos [-1445 -3166 -3]");
+		CalculadoraString.add(" -0 -1001 -2 -2");
+	}*/
+	
+	@Test
+	// Teste para a tarefa 7
+	// Se vier "-0", devo retornar 0 ou mando uma exceção com a mensagem "negativos proibidos [-0]"
+	public void WhenMinusZeroThenEqualsZero() {
+		assertEquals(CalculadoraString.add("-0"),0);
+		assertEquals(CalculadoraString.add("-  0"),0);
 	}
 }
--- labs/Lab02/Lab2_TDD_Pair_Programming/src/CalculadoraString.java
+++ labs/Lab02/Lab2_TDD_Pair_Programming/src/CalculadoraString.java
@@ -1,22 +1,67 @@
 import java.lang.Character;
 
 public class CalculadoraString {
-	static char delimitadorAdicional;
+	static String[] delimitadorAdicional;
+	static int numeroDelimitadores;
 
 	// Verifica se caracter é "," ou " "
 	static String removeZeros(String numbers) {
 		int n = numbers.length();
-		if(!numbers.equals("") && numbers.charAt(0) == '0')
-			return removeZeros(numbers.substring(1,n));
+		if(!numbers.equals("")){
+			if(numbers.charAt(0) == '0')
+				return removeZeros(numbers.substring(1,n));
+		}
 		return numbers;
 	}
 
 	// Verifica se caracter é "," ou " "
-	static boolean verificaSeparador(char c) {
-		boolean separador = false;
-		if(c == ','  || c == ' ' || c == '\n' || c == delimitadorAdicional)
-			separador = true;
-		return separador;
+	static int verificaSeparador(String numbers) {
+		char c = numbers.charAt(0);
+		if(c == ','  || c == ' ' || c == '\n')
+			return 1;
+		for(int i = 0; i<numeroDelimitadores; i++){
+			if(numbers.length() >= delimitadorAdicional[i].length() && numbers.substring(0,delimitadorAdicional[i].length()).equals(delimitadorAdicional[i])){
+				return delimitadorAdicional[i].length();
+			}
+		}
+		return -1;
+	}
+
+	// Adiciona delimitadores
+	static int adicionaSeparador(String numbers) {
+		if (numeroDelimitadores == 0)
+			return 0;
+
+		int aux = 0;
+		int posicao = 0;
+		int fim = posicao+3;
+
+		if(numbers.substring(posicao,fim).equals("//[")){
+			while(numbers.charAt(fim+1) != ']'){
+				fim++;
+			}
+			delimitadorAdicional[0] = numbers.substring(posicao+3,fim+1);
+			posicao = fim+2;
+			aux++;
+		}else{
+			throw new IllegalArgumentException("Entrada inválida e não-tratável");
+		}
+
+		for(int i = 1; i<numeroDelimitadores; i++){
+			fim = posicao+1;
+			if(numbers.charAt(posicao) == '['){
+				while(numbers.charAt(fim+1) != ']'){
+					fim++;
+				}
+				delimitadorAdicional[i] = numbers.substring(posicao+1,fim+1);
+				//System.out.println("delimitadorAdicional[" + i + "] = " + delimitadorAdicional[i]);
+				posicao = fim+2;
+				aux++;
+			}else{
+				throw new IllegalArgumentException("Entrada inválida e não-tratável");
+			}
+		}
+		return posicao;
 	}
 
 	// Função principal
@@ -24,15 +69,23 @@ public class CalculadoraString {
 		int n = numbers.length();
 		if(numbers.equals(""))
 			return 0;
-		delimitadorAdicional = ' ';
-		if(n >= 5){
-			if(numbers.substring(0,3).equals("//[") && numbers.charAt(4) == ']'){
-				delimitadorAdicional = numbers.charAt(3);
-				return q0(numbers.substring(5,n), 0);
-			}else
-				return q0(numbers, 0);
+
+		numeroDelimitadores = (numbers.length() - numbers.replaceAll("]", "").length());
+		delimitadorAdicional = new String[numeroDelimitadores];
+
+		int atual = adicionaSeparador(numbers);
+		for(int i = 0; i<numeroDelimitadores; i++){
+			//System.out.println("delimitadorAdicional[" + i + "] = " + delimitadorAdicional[i]);
+			if(delimitadorAdicional[i].equals("[") || delimitadorAdicional[i].equals("]") || Character.isDigit(delimitadorAdicional[i].charAt(0)))
+				throw new IllegalArgumentException("Entrada inválida e não-tratável");
+			else if(delimitadorAdicional[i].equals("-"))
+				numbers = numbers.replaceAll("-", " ");
 		}
-		return q0(numbers, 0);
+
+		if(atual > 1)
+			return q0(numbers.substring(atual,n), 0);
+		else
+			return q0(numbers, 0);
 	}
 
 	// Estado 0 (inicial e recebe Separadores)
@@ -42,14 +95,21 @@ public class CalculadoraString {
 
 		if(numbers.equals(""))
 			return total;
-		else if( verificaSeparador(numbers.charAt(0)) )
-			return q0(numbers.substring(1,n), total);
+		else if( verificaSeparador(numbers) >= 0)
+			return q0(numbers.substring(verificaSeparador(numbers),n), total);
 		else if( Character.isDigit(numbers.charAt(0)) )
 			return q1(numbers.substring(1,n), numbers.charAt(0) - '0', total);
-		else if( numbers.charAt(0) == '-' && !removeZeros(numbers.substring(1,n)).equals("") && Character.isDigit(removeZeros(numbers.substring(1,n)).charAt(0)) )
-			return q2( removeZeros(numbers.substring(1,n)).substring(1,removeZeros(numbers.substring(1,n)).length()), "[-" + removeZeros(numbers.substring(1,n)).charAt(0) );
+		else if( numbers.charAt(0) == '-' ) {
+			if(!removeZeros(numbers.substring(1,n)).equals("") && Character.isDigit(removeZeros(numbers.substring(1,n)).charAt(0)) )
+				return q2( removeZeros(numbers.substring(1,n)).substring(1,removeZeros(numbers.substring(1,n)).length()), "[-" + removeZeros(numbers.substring(1,n)).charAt(0) );
+			else if(removeZeros(numbers.substring(1,n)).equals(""))
+				return total;
+			else if(verificaSeparador(removeZeros(numbers.substring(1,n))) >= 0)
+				return q0(removeZeros(numbers.substring(1,n)), total);
+		}
 		else
 			throw new IllegalArgumentException("Entrada inválida e não-tratável");
+		return 0;
 	}
 
 	// Estado 1 (Lê a sequência de números e retorna o valor)
@@ -57,12 +117,24 @@ public class CalculadoraString {
 		//System.out.println("q1  " + numbers + "  total = " + total);
 		int n = numbers.length();
 
-		if(numbers.equals(""))
-			return total + atual;
-		else if(verificaSeparador(numbers.charAt(0)))
-			return q0(numbers.substring(1,n), total + atual);
-		else if(Character.isDigit(numbers.charAt(0)))
-			return q1(numbers.substring(1,n), atual*10 + numbers.charAt(0) - '0', total);
+		if(numbers.equals("")){
+			if(atual > 1000)
+				return total;
+			else 
+				return total + atual;
+		}
+		else if(verificaSeparador(numbers) >= 0){
+			if(atual > 1000)
+				return q0(numbers.substring(verificaSeparador(numbers),n), total);
+			else 
+				return q0(numbers.substring(verificaSeparador(numbers),n), total + atual);
+		}
+		else if(Character.isDigit(numbers.charAt(0))){
+			if(atual > 1000)
+				return q1(numbers.substring(1,n), 1001, total);
+			else 
+				return q1(numbers.substring(1,n), atual*10 + numbers.charAt(0) - '0', total);
+		}
 		else
 			throw new IllegalArgumentException("Entrada inválida e não-tratável");
 	}
@@ -74,9 +146,9 @@ public class CalculadoraString {
 
 		if(numbers.equals(""))
 			throw new IllegalArgumentException("negativos proibidos " + negativos + "]");
-		else if(verificaSeparador(numbers.charAt(0)))
-			return q3( numbers.substring(1,n), negativos);
-		else if( Character.isDigit(numbers.charAt(0)) )
+		else if(verificaSeparador(numbers) >= 0)
+			return q3( numbers.substring(verificaSeparador(numbers),n), negativos);
+		else if( Character.isDigit(numbers.charAt(0)))
 			return q2( numbers.substring(1,n), negativos + numbers.charAt(0) );
 		else
 			throw new IllegalArgumentException("Entrada inválida e não-tratável");
@@ -84,11 +156,14 @@ public class CalculadoraString {
 
 	// Estado 3 (recebe Separadores do estado 2)
 	static int q3(String numbers, String negativos) {
+		//System.out.println("q3  " + numbers);
 		int n = numbers.length();
 
 		if(numbers.equals(""))
 			throw new IllegalArgumentException("negativos proibidos " + negativos + "]");
-		else if(verificaSeparador(numbers.charAt(0)) || Character.isDigit(numbers.charAt(0)))
+		else if(verificaSeparador(numbers) >= 0)
+			return q3( numbers.substring(verificaSeparador(numbers),n), negativos);
+		else if(Character.isDigit(numbers.charAt(0)))
 			return q3( numbers.substring(1,n), negativos);
 		else if( numbers.charAt(0) == '-' && !removeZeros(numbers.substring(1,n)).equals("") && Character.isDigit(removeZeros(numbers.substring(1,n)).charAt(0)) )
 			return q2( removeZeros(numbers.substring(1,n)).substring(1,removeZeros(numbers.substring(1,n)).length()), negativos + " -" + removeZeros(numbers.substring(1,n)).charAt(0) );
