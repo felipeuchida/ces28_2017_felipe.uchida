d9b0c9e9d2f4768f5d6ed3744c3b32aa56d70e8d
felipeuchida <felipeuchida1@gmail.com>
2017-09-19 10:35:30 -0300

    Adicionar lista de sala - A10_DP_SingTempCompFaca
    CompositeDesignPattern atualizado.

--- listas_sala/A10_DP_SingTempCompFaca/TemplateMethodDesignPattern/src/Drawing.java
+++ listas_sala/A10_DP_SingTempCompFaca/TemplateMethodDesignPattern/src/Drawing.java
@@ -1,14 +1,38 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Drawing implements Shape{
+	private List<Shape> _shapes = new LinkedList<Shape>( );
+	
 	@Override
 	public void draw(String fillColor) {
-		System.out.println("Drawing with color "+fillColor);
+		int n = _shapes.size();
+		for (int i=0; i < n; i++) {
+			_shapes.get(i).draw(fillColor);
+		}
 	}
 
 	public void add(Shape shape) {
-
+		_shapes.add(shape); 
+	}
+	
+	public void remove(int i) {
+		_shapes.remove(i); 
+	}
+	
+	public int size() { 
+		return _shapes.size(); 
+	}
+	
+	public Shape get(int i) { 
+		return _shapes.get(i); 
 	}
 	
 	public void clear() {
-
+		System.out.println("Clearing all the shapes from drawing");
+		int n = _shapes.size();
+		for (int i=n-1; i >= 0; i--) {
+			_shapes.remove(i);
+		}
 	}
 }
